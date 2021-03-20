e81078990bdaa2181d0922866ddb129413e30168
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2021-03-20 14:16:15 +0200

    remove default stuff and add some new

---
+++ src/views/Game.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span>Boxes left: {{boxesLeft}}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Game',
+  components: {
+  },
+  data() {
+    return {
+      boxesLeft: 2
+    }
+  },
+  methods: {
+    boxPlaced() {
+      console.log("box placed");
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/MainMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>CAT PUZZLE BOX GAME</h1>
+    <router-link to="/game">Play</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainMenu'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
