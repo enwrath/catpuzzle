a7deabcbaa513cc15c2baa66d16435c5f2b930cf
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2021-04-08 20:57:33 +0300

    initial layouting for level select, needs images

--- src/views/LevelSelect.vue
+++ src/views/LevelSelect.vue
@@ -3,15 +3,21 @@
     <router-link to="/">Back to main menu</router-link>
     <div v-if="worldSelected === ''">
       <h1>Select World</h1>
-      <div :key="`world-${world.id}`" v-for="world in levels" >
-        <button @click="worldSelected=world.id">{{world.name}}</button>
+      <div class="worldContainer">
+        <div :key="`world-${world.id}`" v-for="(world, i) in levels" class="worldSelect" :style="`margin-left: ${i * 2 + 1}em;`">
+          <button class="bigbutton" @click="worldSelected=world.id">{{world.name}}</button>
+        </div>
       </div>
     </div>
     <div v-else>
       <button @click="worldSelected=''">Back to world selection</button>
       <h1>Select Level</h1>
-      <div :key="`level-${level.id}`" v-for="level in levels[worldSelected].levels" >
-        <router-link :to="`/game/${levels[worldSelected].id}-${level.id}`">{{level.name}}</router-link>
+      <div class="levelContainer">
+        <div :key="`level-${level.id}`" v-for="(level, i) in levels[worldSelected].levels" class="levelSelect" >
+          <router-link :to="`/game/${levels[worldSelected].id}-${level.id}`" custom v-slot="{ navigate }">
+            <button class="bigbutton" @click="navigate">{{i}}</button>
+          </router-link>
+        </div>
       </div>
     </div>
 
@@ -43,4 +49,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.worldContainer {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+.worldSelect {
+  width: 60%;
+  height: 4em;
+  margin-bottom: 0.25em;
+}
+.levelContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  max-width: 30em;
+  margin:auto;
+}
+.levelSelect {
+  width: 4em;
+  height: 4em;
+  margin: 0.25em;
+}
+.bigbutton {
+  width: 100%;
+  height: 100%;
+}
 </style>
