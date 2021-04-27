1f71f4c25748e5b3920e59d1d2c7672a176df01e
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2021-04-27 21:44:19 +0300

    remove cat image from message

--- src/components/LevelMessage.vue
+++ src/components/LevelMessage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="main">
-    <img :src="require(`@/assets/cat.webp`)" />
     <div class="bubble">
       <span>{{ message }}</span>
     </div>
@@ -21,37 +20,18 @@ export default {
 .main {
   display: flex;
   flex-direction: row;
-  background: #353558;
-  padding-right: 1em;
   justify-content: center;
   align-items: center;
-  margin-right: 4rem;
+  padding-right: 4rem;
 }
 .bubble {
   position: relative;
-	background: #c7ecd2;
-	border-radius: .4em;
+	background: #cce2c6;
+	border-radius: .8em;
   display: inline-block;
   display: flex;
   align-items: center;
   margin: 0.5em 0 0.5em 1em;
   padding: 1em;
-}
-
-.bubble:after {
-	content: '';
-	position: absolute;
-	left: 0;
-	top: 50%;
-	width: 0;
-	height: 0;
-	border: 1em solid transparent;
-	border-right-color: #c7ecd2;
-	border-left: 0;
-	margin-top: -1em;
-	margin-left: -1em;
-}
-img {
-  max-height: 5em;
 }
 </style>
